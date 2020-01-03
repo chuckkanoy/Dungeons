14238df4b4919e6ee4884cb6f7789ce730c081bf
chuckkanoy <ch.kanoy@gmail.com>
2020-01-03 14:05:33 -0600

    made sprites and added features

--- Dungeons0.1.py
+++ Dungeons0.1.py
@@ -5,7 +5,7 @@
 import pygame
 import sys
 from numpy import *
-from threading import Timer
+import threading
 import tkinter as tk
 from operator import itemgetter
 # from iteration_utilities import chained
@@ -19,9 +19,9 @@ pygame.display.set_caption("Dungeons")
 
 
 # play background music
-"""pygame.mixer.music.load("Data/labyrinth-of-time.mp3")
+pygame.mixer.music.load("Data/labyrinth-of-time.mp3")
 pygame.mixer.music.set_volume(.1)
-pygame.mixer.music.play(-1)"""
+pygame.mixer.music.play(-1)
 
 # color palette
 black = (0, 0, 0)
@@ -44,12 +44,14 @@ scale = 10
 square = height / scale
 
 # global game variables
+num_barriers = 0
 level_count = 1
 points = 0
-adder = 0
+adder = 1
 health = 5
 playerName = ""
 board = [[0 for x in range(10)] for y in range(10)]
+num_possible_power_ups = 2
 
 
 # initialize array board
@@ -68,6 +70,7 @@ def print_board():
 
 # main method called to begin game actions
 def main():
+    # show_scores()  # use temporarily for scoreboard modification
     global playerName
     # display opening screen
     screen.fill(black)
@@ -84,17 +87,14 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     sys.exit()
                 elif event.key == pygame.K_SPACE:
-                    game_setup()
+                    # move on to next step if continue occurs
+                    get_init()
 
 
 # initialize game space and playerName
 def get_init():
     # get player initials
     global playerName
-    # display opening screen
-    screen.fill(black)
-    text_to_screen(screen=screen, text="Enter Initials", x=40, y=250, color=white)
-    pygame.display.flip()
 
     # allow player to only enter 3 characters for initials
     while len(playerName) != 3:
@@ -106,7 +106,7 @@ def get_init():
                 if event.key == pygame.K_ESCAPE:
                     sys.exit()
                 elif event.unicode.isalpha():
-                    playerName += event.unicode
+                    playerName += event.unicode.upper()
                 elif event.key == pygame.K_BACKSPACE:
                     playerName = playerName[:-1]
                 # show key input as it happens on screen
@@ -121,25 +121,52 @@ def get_init():
 
 # setup game variables
 def game_setup():
-    # add base-game components
-    door = Door(random.randint(0, scale - 1), random.randint(0, scale - 1), black)
+    global level_count
+    # add player component
     gamer = Player(random.randint(0, scale - 1), random.randint(0, scale - 1), 1, horse_brown)
+
+    door = Door(random.randint(0, scale - 1), random.randint(0, scale - 1), black)
+    while gamer.x == door.x and gamer.y == door.y:
+        door = Door(random.randint(0, scale - 1), random.randint(0, scale - 1), black)
+    # add power up depending on random
+    appears = random.randint(0, 100)
+    if 0 <= appears <= 50:
+        powerup = []
+    else:
+        choice = random.randint(0, num_possible_power_ups)
+        if choice == 0:
+            powerup = [Health()]
+        else:
+            powerup = [Treasure()]
 
     # add enemies
     vill = []
-    for i in range(adder):
-        enemy = Enemy(random.randint(0, scale - 1), random.randint(0, scale - 1), 1, 1, red, adder)
-        vill.append(enemy)
+    if level_count != 1:
+        for i in range(adder):
+            enemy = Enemy(random.randint(0, scale - 1), random.randint(0, scale - 1), 1, 1, red, adder)
+            vill.append(enemy)
 
-    game_play(gamer, vill, door)
+    barrier = Barrier()
+    # for j in range(num_barriers):
+    if level_count == 1:
+        for i in range(adder):
+            while (gamer.x == barrier.x and gamer.y == barrier.y) or (door.x == barrier.x and door.y == barrier.y):
+                barrier = Barrier()
+    else:
+        for i in range(adder):
+            while (vill[i].x == barrier.x and vill[i].y == barrier.y) or \
+                    (gamer.x == barrier.x and gamer.y == barrier.y) or (door.x == barrier.x and door.y == barrier.y):
+                barrier = Barrier()
+
+    game_play(gamer, vill, door, powerup, barrier)
 
 
 # run game when player indicates
-def game_play(player, enemy, door):
-    global adder
-    global health
-    # show_scores() NO NEED TO SHOW HIGH SCORES BEFORE GAME STARTS
-    print_board()
+def game_play(player, enemy, door, powerup, barrier):
+    # global variables
+    global num_barriers
+
+    # print_board() used when displaying array being saved
     while health > 0:
         # handle every event
         for event in pygame.event.get():
@@ -159,23 +186,39 @@ def game_play(player, enemy, door):
                 elif event.key == pygame.K_SPACE:
                     player.fire(enemy, door)
                 else:
-                    player.move_player()
-                for i in range(adder):
-                    enemy[i].move(player)
-                    if player.x == enemy[i].x and player.y == enemy[i].y:
-                        health -= 1
-                        enemy.remove(enemy[i])
-                        adder -= 1
-                        break
-                print_screen(screen, player, door, enemy)
-                board_init()
+                    # for i in range(num_barriers):
+                    player.move_player(barrier)
+                    if len(powerup) != 0:
+                        if powerup[0].check_collision(player):
+                            powerup[0].boost(player)
+                            powerup.remove(powerup[0])
+                    # for i in range(num_barriers):
+                    run_enemy(enemy, player, barrier)
+                print_screen(screen, player, door, enemy, powerup, barrier)
+                # board_init() used when displaying array being saved
     game_over()
+
+
+# handle timed enemy
+def run_enemy(enemy, player, barrier):
+    # global variables
+    global health
+    global level_count
+
+    if level_count != 1:
+        for i in range(len(enemy)):
+            enemy[i].move(player, barrier)
+            if player.x == enemy[i].x and player.y == enemy[i].y:
+                health -= 1
+                enemy.remove(enemy[i])
+                break
 
 
 # handle what happens upon player death
 def game_over():
+    # display sorted scores
+    sort_scores()
     write_score()
-    # sort_scores() NO NEED TO SORT SCORE; WILL ALREADY BE DONE IN SHOW_SCORE
 
     # declare and adjust global variables
     global health
@@ -185,6 +228,7 @@ def game_over():
     points = 0
     health = 5
 
+    # display game over screen
     screen.fill(black)
     text_to_screen(screen, "Game Over", 90, 250, 50, white)
     text_to_screen(screen, "Press space to continue", 100, 400, 20, white)
@@ -214,28 +258,31 @@ def show_level():
 
     # increase adder appropriately
     global adder
-    if (level_count % 1) == 0:
-        adder = level_count
+    if (level_count % 5) == 0:
+        adder += 1
 
     screen.fill(black)
     text_to_screen(screen=screen, text="Level " + str(level_count), x=140, y=250, color=white)
     text_to_screen(screen, "Press space to continue", 100, 400, 20, white)
     pygame.display.flip()
 
-    await_key()
+    await_continue()
 
 
-# UNFINISHED: sort scores in text document
+# sort scores in text document
 def sort_scores():
-    with open("Data/scores.txt") as f:
-        lines = [line.split(' ') for line in f]
-    output = open("Data/scores.txt", "w")
+    # declare local variables for use
+    file = open("Data/scores.txt", "r")
+    lines = file.readlines()
+    file.close()
 
-    itemgetter_int = chained(operator.itemgetter(0, 1), partial(map, int), tuple)
-    for line in sorted(lines, key=itemgetter_int(1), reverse=True):
-        output.write(' '.join(line))
+    # sort the list of lines read
+    lines.sort(key = lambda x: (len(x), x), reverse = True)
 
-    output.close()
+    # write the sorted list to file
+    file = open("Data/scores.txt", "w")
+    for i in range(5):
+        file.write(lines[i])
 
 
 # write scores to a text document
@@ -244,19 +291,22 @@ def write_score():
     global playerName
 
     file = open("Data/scores.txt", "a")
-    file.write(playerName + " " + str(points) + "\n")
+    file.write(str(points) + " " + playerName + "\n")
 
 
 # display scores to screen
 def show_scores():
-    # sort_scores()
+    # sort the scores before showing
+    write_score()
+    sort_scores()
+
     # read in file
     f = open("Data/scores.txt")
     line = f.readline()
 
     # fill in screen with details
     screen.fill(black)
-    text_to_screen(screen=screen, text="Scores", x=130, y=100, color=white)
+    text_to_screen(screen=screen, text="High Scores", x=70, y=100, color=white)
     height_win = 180
     for i in range(5):
         text_to_screen(screen=screen, text=line.replace("\n", ""), x=140, y=height_win, color=white)
@@ -265,11 +315,11 @@ def show_scores():
     text_to_screen(screen, "Press space to continue", 100, 600, 20, white)
     pygame.display.flip()
 
-    await_key()
+    await_continue()
 
 
 # waits for player to enter a specific key to followup with action
-def await_key():
+def await_continue():
     while 1:
         # quit if necessary
         for event in pygame.event.get():
@@ -279,21 +329,28 @@ def await_key():
                 if event.key == pygame.K_ESCAPE:
                     sys.exit()
                 elif event.key == pygame.K_SPACE:
-                    game_setup()
+                    get_init()
 
 
 # prints objects on the screen
-def print_screen(screen, player, door, enemy):
+def print_screen(screen, player, door, enemy, powerup, barrier):
     global level_count
     global playerName
 
     screen.fill(green)
     player.draw()
     door.draw()
-    for i in range(adder):
-        enemy[i].draw()
+    if len(powerup) != 0:
+        powerup[0].draw()
 
-    print_board()
+    if level_count != 1:
+        for i in range(len(enemy)):
+            enemy[i].draw()
+
+    """for i in range(num_barriers):
+        print(num_barriers + " " + i)
+        barriers[i].draw()"""
+    barrier.draw()
 
     # draw player UI on bottom of screen
     outline = pygame.rect.Rect(0, height, width, 200)
@@ -352,27 +409,27 @@ class Player(GameObject):
         screen.blit(image, self.rect)
 
     # handle what happens when player pushes keys
-    def move_player(self):
+    def move_player(self, barrier):
         actual = self.vel * square
 
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
-            if not (self.x <= 0):
+            if not (self.x <= 0) and (self.y != barrier.y or self.x - 1 != barrier.x):
                 self.rect.move_ip(-actual, 0)
                 self.x -= self.vel
                 self.face = LEFT
         elif key[pygame.K_RIGHT]:
-            if not (self.x >= scale - self.vel):
+            if not (self.x >= scale - self.vel) and (self.y != barrier.y or self.x + 1 != barrier.x):
                 self.rect.move_ip(actual, 0)
                 self.x += self.vel
                 self.face = RIGHT
         elif key[pygame.K_UP]:
-            if not (self.y <= 0):
+            if not (self.y <= 0) and (self.y - 1 != barrier.y or self.x != barrier.x):
                 self.rect.move_ip(0, -actual)
                 self.y -= self.vel
                 self.face = UP
         elif key[pygame.K_DOWN]:
-            if not (self.y >= scale - self.vel):
+            if not (self.y >= scale - self.vel) and (self.y + 1 != barrier.y or self.x != barrier.x):
                 self.rect.move_ip(0, actual)
                 self.y += self.vel
                 self.face = DOWN
@@ -416,7 +473,7 @@ class Player(GameObject):
         clock = pygame.time.Clock()
 
         global screen
-        print_screen(screen, self, door, enemy)
+        # print_screen(screen, self, door, enemy) make animation
 
         ice_block = pygame.rect.Rect(ice_x * square, ice_y * square, square, square)
 
@@ -427,14 +484,14 @@ class Player(GameObject):
 
     # checks for collisions
     def check_collision(self, enemy, ice_x, ice_y):
-        global adder
         global points
-        for i in range(adder):
-            if int(enemy[i].x) == ice_x and int(enemy[i].y) == ice_y:
-                enemy.remove(enemy[i])
-                adder -= 1
-                points += 1
-                return True
+        global level_count
+        if level_count != 1:
+            for i in range(len(enemy)):
+                if int(enemy[i].x) == ice_x and int(enemy[i].y) == ice_y:
+                    enemy.remove(enemy[i])
+                    points += 1
+                    return True
 
 
 # class for movement between levels
@@ -444,7 +501,8 @@ class Door(GameObject):
 
     # draws door (hole)
     def draw(self):
-        pygame.draw.ellipse(screen, self.color, self.rect)
+        image = pygame.image.load('Data/door.png')  # adjust as needed if file changes
+        screen.blit(image, self.rect)
         # change board variable
         board[self.y][self.x] = 2
 
@@ -478,32 +536,91 @@ class Enemy(GameObject):
 
         screen.blit(image, self.rect)
 
-    # UNFINISHED: handle enemy pursuing player; want it timed
-    def move(self, player):
+    # moves individual enemies
+    def move(self, player, barrier):
         actual = self.vel * square
         x_dist = abs(self.x - player.x)
         y_dist = abs(self.y - player.y)
 
         if x_dist > y_dist:
-            if self.x > player.x:
+            if self.x > player.x and (self.x - 1 != barrier.x or self.y != barrier.y):
                 self.rect.move_ip(-actual, 0)
                 self.x -= self.vel
                 self.face = LEFT
-            elif self.x < player.x:
+            elif self.x < player.x and (self.x + 1 != barrier.x or self.y != barrier.y):
                 self.rect.move_ip(actual, 0)
                 self.x += self.vel
                 self.face = RIGHT
         else:
-            if self.y > player.y:
+            if self.y > player.y and (self.x != barrier.x or self.y - 1 != barrier.y):
                 self.rect.move_ip(0, -actual)
                 self.y -= self.vel
                 self.face = UP
-            elif self.y < player.y:
+            elif self.y < player.y and (self.x != barrier.x or self.y + 1 != barrier.y):
                 self.rect.move_ip(0, actual)
                 self.y += self.vel
                 self.face = DOWN
 
         board[self.y][self.x] = 3
+
+
+# class for barriers
+class Barrier(GameObject):
+    def __init__(self):
+        GameObject.__init__(self, random.randint(0, scale - 1), random.randint(0, scale - 1), black)
+
+    def draw(self):
+        image = pygame.image.load('Data/boulder.png')  # adjust as needed if file changes
+        screen.blit(image, self.rect)
+
+
+# class for power ups in game
+class PowerUp(GameObject):
+    def __init__(self, color):
+        GameObject.__init__(self, random.randint(0, scale - 1), random.randint(0, scale - 1), color)
+
+    # checks for collisions
+    def check_collision(self, player):
+        if self.x == player.x and self.y == player.y:
+            return True
+
+
+# class for health power up
+class Health(PowerUp):
+    def __init__(self):
+        PowerUp.__init__(self, red)
+
+    def draw(self):
+        image = pygame.image.load('Data/heart.png')  # adjust as needed if file changes
+        screen.blit(image, self.rect)
+        # change board variable
+        board[self.y][self.x] = 5
+
+    # cause special effect on player variable
+    def boost(self, player):
+        global health
+
+        if self.check_collision(player):
+            health += 1
+
+
+# class for treasure power up
+class Treasure(PowerUp):
+    def __init__(self):
+        PowerUp.__init__(self, horse_brown)
+
+    def draw(self):
+        image = pygame.image.load('Data/chest.png')  # adjust as needed if file changes
+        screen.blit(image, self.rect)
+        # change board variable
+        board[self.y][self.x] = 6
+
+    # cause special effect on player variable
+    def boost(self, player):
+        global points
+
+        if self.check_collision(player):
+            points += 5
 
 
 main()
